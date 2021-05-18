76e0d62896e951c535f5d8b577f6ae36a41369c7
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-05-18 14:10:11 +0430

    Update SimpleClientSock.cpp

--- SimpleClientSock.cpp
+++ SimpleClientSock.cpp
@@ -1,6 +1,6 @@
 #include<WS2tcpip.h>
 #include <iostream>
-
+#include <string>
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 using namespace std;
 
@@ -10,62 +10,74 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	// INITIALIZE Win Socket
 	WSADATA Win_Socket_Info;
 	SOCKET MyConnection;
-	struct sockaddr_in Server_Info;
 
-	printf("\nInitialising Winsock...");
 	if (WSAStartup(MAKEWORD(2, 2), &Win_Socket_Info) != 0)
 	{
 		cerr << "Failed. Error Code : " << WSAGetLastError();
 		return 1;
 	}
-
 	cout << "\nInitialised...";
 
-	//Create a socket
+	// CREATE SOCKET
 	if ((MyConnection = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
 	{
 		cout << "\nCould not create socket :" << WSAGetLastError();
+		WSACleanup();
 	}
-
 	cout << "\nSocket created...";
 
-
+	// FILL IN HINT STRUCTURE 
+	sockaddr_in Server_Info;
 	Server_Info.sin_family = AF_INET;
 	Server_Info.sin_port = htons(ServerPORT);
 	if (inet_pton(AF_INET, ServerIP, &Server_Info.sin_addr) <= 0)
 	{
 		cerr << "Failed. Error Code : " << WSAGetLastError();
+		WSACleanup();
 		return 1;
 	}
 
-	//Connect to remote server
+	//CONNECT TO SERVER
 	while (true)
 	{
-		if (connect(MyConnection, (struct sockaddr*)&Server_Info, sizeof(Server_Info)) < 0)
-			cerr << "\nconnection error ... !";
+		int connectionResult = connect(MyConnection, (struct sockaddr*)&Server_Info, sizeof(Server_Info));
+		if (connectionResult == SOCKET_ERROR)
+		{
+			cerr << "\nCan not Connect to server ... !";
+		}
 		else
 			break;
 		Sleep(2 * 1000);
 	}
-	
+
 
 	cout << "\nConnected\n";
 
 	//Send some data
-	
+	char Buffer[BUFFERLEN];
+	string MESSAGE;
 	while (true)
 	{
-		char Buffer[BUFFERLEN];
 		cout << "\n-> Write your Message : ";
-		std::cin >> Buffer;
-		if (send(MyConnection, Buffer, strlen(Buffer) , 0) < 0)
+		std::getline(std::cin, MESSAGE);
+		if (!MESSAGE.empty())
 		{
-			puts("\nSend failed");
-			return 1;
+			if (send(MyConnection, MESSAGE.c_str() , MESSAGE.length() + 1 , 0) < 0)
+			{
+				cerr << ("\nSend failed");
+				return 1;
+			}
+			ZeroMemory(Buffer, BUFFERLEN);
+			int byteRecieved = recv(MyConnection, Buffer, BUFFERLEN, 0);
+			if (byteRecieved > 0)
+			{
+				cout << "Server > " << string(Buffer , byteRecieved);
+			}
+			
 		}
-		puts(">>>>> Data Send\n");
 	}
 
 	return 0;
